a7f63d4e5a485c29a077b4c4558b71864905f936
Thanh Pham <khanhthy@yahoo.com>
2025-01-21 11:48:58 -0800

    Create MovieCard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+export default function MovieCard({movie}){
+
+    function onfavoritesClick(){
+        alert("Click!")
+    }
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-button" onClick={onfavoritesClick}></button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
