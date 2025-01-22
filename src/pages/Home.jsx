df8c411c51fef0714c5fe7472479fcf085a3d12c
Thanh Pham <khanhthy@yahoo.com>
2025-01-22 10:13:43 -0800

    Update Home component and create mew Favorites component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
 import MovieCard from "../components/MovieCard"
 
+
 export default function Home(){ 
+
+    const [searchQuery, setSearchQuery] = useState("")
 
     const movies = [
         { id: 1, title: "John Wick", release_date: "2020"},
@@ -8,14 +12,28 @@ export default function Home(){
         { id: 3, title: "The Matrix", release_date: "1998"}
     ]
 
+    const onHandleSearch = () => {
+        alert(searchQuery)
+    }
+
     return (
         <div className="home">
+            <form onSubmit={onHandleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            
             <div className="movies-grid">
-                    { movies.map( 
-                        (movie) => 
-                            ( <MovieCard movie={movie} key={movie.id}/>)
-                    )
-                    }   
+                    { movies.map((movie) => 
+                            ( <MovieCard movie={movie} key={movie.id}/>
+
+                   ))}   
             </div>
         </div>
     )
