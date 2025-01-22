407838f34476aa09e9513526f7e149d7a45c1971
Thanh Pham <khanhthy@yahoo.com>
2025-01-22 11:20:12 -0800

    Update Add, Main component for router

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom"
+import Favorites from './pages/Favorites'
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path='/' element={<Home />}>Home</Route>
+        <Route path='/favorites' element={<Favorites />}>Favorites</Route>
+      </Routes>
+    </main>
   )
 }
 
