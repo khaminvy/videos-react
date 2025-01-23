b7c12e127a424b17f7944e07071bf8460becf982
Thanh Pham <khanhthy@yahoo.com>
2025-01-23 11:03:22 -0800

    Create new folder service with file api.js for creating api service

---
+++ src/service/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "d03ef799203b43c38a80eaa6ca1a6e33";
+const BASE_URL =  "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = response.json();
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = response.json();
+    return data.results
+}
